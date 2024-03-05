7d24d23f8ede494cd381148496a9431a46196ebd
Bunyamin Kalkan <bnkalkan41@gmail.com>
2024-03-05 14:45:19 +0300

    feat: database conneciton done

---
+++ src/main/java/com/bunyaminkalkan/asksomeone/AsksomeoneApplication.java
@@ -0,0 +1,13 @@
+package com.bunyaminkalkan.asksomeone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsksomeoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsksomeoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bunyaminkalkan/asksomeone/AsksomeoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bunyaminkalkan.asksomeone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsksomeoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
